a7ff2ed9e7c10b6462dcf3fe186f46711ec02334
derekgm <derekgm@byu.local@acme40.byu.edu>
2016-10-13 12:57:20 -0600

    added acm hard problems

---
+++ practice/acm_EKICG.py
@@ -0,0 +1,18 @@
+# code file for problems EKICG
+
+def probE(test_input):
+    ship_vars = dict()
+    ship = 
+    return 0
+
+def probK():
+    return 0
+
+def probI():
+    return 0
+
+def probC():
+    return 0
+
+def probG():
+    return 0
